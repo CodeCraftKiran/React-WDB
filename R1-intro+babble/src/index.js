13bd7c6fabfd9e6a7e4d317081bb07ad0d425b0c
CodeCraftKiran <codecraftkiran@gmail.com>
2024-12-07 20:56:33 +0530

    React intro + bable

---
+++ R1-intro+babble/src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+ReactDom.render(
+  <>
+    <h1>Hello World!</h1>
+    <p>This is a paragraph</p>
+  </>,
+  document.getElementById("root")
+);
